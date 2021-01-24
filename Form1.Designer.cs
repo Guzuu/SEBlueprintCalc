6610cb97905ababda9c331a569d4342dbe78cb97
Guzuu <mateuszguz54@gmail.com>
2021-01-24 17:52:00 +0100

    #1 Added main form and methods

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SEBlueprintCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(357, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open blueprint";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(339, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(427, 293);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(186, 418);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(309, 450);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(192, 424);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Components";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(301, 424);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Blocks";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.GridColor = System.Drawing.SystemColors.ControlLight;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(295, 418);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(565, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 61);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Space Engineers location";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "SE-Blueprint-Calc";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace SEBlueprintCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = ".bp file";
+            openFileDialog1.ShowDialog();
+
+            try
+            {
+                var bpFile = File.ReadAllText(openFileDialog1.FileName);
+                pictureBox1.Image = Image.FromFile(Path.GetDirectoryName(openFileDialog1.FileName) + "\\thumb.png");
+                Dictionary<string, int> bp = readXMLBlueprint(bpFile);
+                dataGridView1.DataSource = getComponents(bp).ToList();
+                dataGridView2.DataSource = bp.ToList();
+            }
+            catch (Exception ex)
+            {
+                if(ex is XmlException)
+                {
+                    MessageBox.Show("Make sure a .bp file was selected");
+                }
+                else if (ex is FileNotFoundException)
+                {
+                    MessageBox.Show("File was not selected");
+                }
+                return;
+            }
+        }
+
+        public Dictionary<string, int> readXMLBlueprint(string file)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            string key;
+            XmlDocument bp = new XmlDocument();
+
+            bp.LoadXml(file);
+
+            var blocks = bp.DocumentElement.SelectNodes("//CubeBlocks/MyObjectBuilder_CubeBlock/SubtypeName");
+
+            foreach(XmlNode block in blocks)
+            {
+                key = block?.InnerText ?? "";
+                if (key == "") continue;
+                if (dict.ContainsKey(key)) dict[key]++;
+                else dict.Add(key, 1);
+            }
+
+            return dict;
+        }
+
+        public Dictionary<string, Dictionary<string, int>> readXMLBlockInfo(string file, Dictionary<string, Dictionary<string, int>> blockDict)
+        {
+            Dictionary<string, int> compDict = new Dictionary<string, int>();
+            int compCount;
+
+            XmlDocument blocks = new XmlDocument();
+            blocks.LoadXml(file);
+
+            var blockSections = blocks.DocumentElement.SelectNodes("//Definition");
+
+            foreach(XmlNode section in blockSections)
+            {
+                var blockName = section.SelectSingleNode(".//Id/SubtypeId")?.InnerText ??"";
+                var components = section.SelectNodes(".//Components/Component");
+                foreach(XmlElement component in components)
+                {
+                    var compName = component.GetAttribute("Subtype");
+                    int.TryParse(component.GetAttribute("Count"), out compCount);
+
+                    if (compDict.ContainsKey(compName)) compDict[compName] += compCount;
+                    else compDict.Add(compName, compCount);
+                }
+                if (blockDict.ContainsKey(blockName)) continue;
+                else blockDict.Add(blockName, new Dictionary<string, int>(compDict));
+                compDict.Clear();
+            }
+
+            return blockDict;
+        }
+
+        public Dictionary<string, int> getComponents(Dictionary<string, int> bpBlocks)
+        {
+            Dictionary<string, Dictionary<string, int>> blockDict = new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, int> comps = new Dictionary<string, int>();
+            string path = "D:\\Program Files (x86)\\SteamLibrary\\steamapps\\common\\SpaceEngineers\\Content\\Data\\CubeBlocks\\";
+
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Armor.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Armor_2.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Automation.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Communications.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Control.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_DecorativePack.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_DecorativePack2.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Doors.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Economy.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Energy.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Extras.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Frostbite.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Gravity.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Interiors.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_LCDPanels.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Lights.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Logistics.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Mechanical.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Medical.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Production.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_ScrapRacePack.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_SparksOfTheFuturePack.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Symbols.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Thrusters.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Tools.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Utility.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Weapons.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Wheels.sbc"), blockDict);
+            readXMLBlockInfo(File.ReadAllText(path + "CubeBlocks_Windows.sbc"), blockDict);
+
+            foreach (var bpBlock in bpBlocks)
+            {
+                foreach(var comp in blockDict[bpBlock.Key])
+                {
+                    if (comps.ContainsKey(comp.Key)) comps[comp.Key] += comp.Value * bpBlock.Value;
+                    else comps.Add(comp.Key, comp.Value * bpBlock.Value);
+                }
+            }
+
+            return comps;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+        }
+    }
+}
